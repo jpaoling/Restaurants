ab6b5cf36a4c51558ce8367efb431227ab3e94aa
Jonas Makonnen <jpaoling@gmail.com>
2018-08-30 15:56:16 +0200

    ...

--- Projects/EDA/CHIS.R
+++ Projects/EDA/CHIS.R
@@ -221,3 +221,26 @@ p1 %+% DF_all +
             show.legend = FALSE)
 
 
+## Generalizations
+
+# Load all packages
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+library(ggthemes)
+
+# Script generalized into a function
+mosaicGG
+
+# BMI described by age
+mosaicGG(adult, X = "SRAGE_P", FILL = "RBMI")
+
+# Poverty described by age
+mosaicGG(adult, X = "SRAGE_P", FILL = "POVLL")
+
+# mtcars: am described by cyl
+mosaicGG(mtcars, X = "cyl", FILL = "am")
+
+# Vocab: vocabulary described by education
+library(carData)
+mosaicGG(Vocab, X = "education", FILL = "vocabulary")
